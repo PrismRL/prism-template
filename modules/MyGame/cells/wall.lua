c8c3c9e0b6f7c2f5b15fb36e8cb40e9b47912a34
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-03-27 16:24:11 -0400

    Cells in the template still had passable bool.

--- modules/MyGame/cells/wall.lua
+++ modules/MyGame/cells/wall.lua
@@ -2,7 +2,6 @@
 local Wall = prism.Cell:extend("WallCell")
 
 Wall.name = "Wall" -- displayed in the user interface
-Wall.passable = false -- defines whether a cell is passable
 Wall.opaque = true -- defines whether a cell can be seen through
 Wall.drawable = prism.components.Drawable(string.byte("#") + 1)
 
