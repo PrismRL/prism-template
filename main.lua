7dbcffae9845750060dd1d24d27dd2258b633dfd
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-05-09 03:18:26 -0400

    Moved map generation to MyGameLevelState.

--- main.lua
+++ main.lua
@@ -5,18 +5,6 @@ prism.loadModule("prism/spectrum")
 prism.loadModule("modules/Sight")
 prism.loadModule("modules/MyGame")
 
--- Map Generation
-
--- build a basic test map
-local mapbuilder = prism.MapBuilder(prism.cells.Wall)
-mapbuilder:drawRectangle(0, 0, 32, 32, prism.cells.Wall)
-mapbuilder:drawRectangle(1, 1, 31, 31, prism.cells.Floor)
-mapbuilder:drawRectangle(5, 5, 7, 7, prism.cells.Wall)
-mapbuilder:drawRectangle(20, 20, 25, 25, prism.cells.Pit)
-
--- create and add the player
-mapbuilder:addActor(prism.actors.Player(), 12, 12)
-
 -- Grab our level state and sprite atlas.
 local MyGameLevelState = require "gamestates.MyGamelevelstate"
 
