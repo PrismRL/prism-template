a6238162412306aeab84ed3cd42b5d5a5a21d1c5
LJNIC <liamreadsspam@gmail.com>
2025-11-29 11:45:49 -0400

    Update to prism 1.1

--- modules/game/actions/move.lua
+++ modules/game/actions/move.lua
@@ -1,18 +1,16 @@
-local MoveTarget = prism.Target()
-   :isPrototype(prism.Vector2)
-   :range(1)
+local MoveTarget = prism.Target():isVector2():range(1)
 
----@class Move : Action
----@field name string
----@field targets Target[]
----@field previousPosition Vector2
+--- @class Move : Action
+--- @field name string
+--- @field targets Target[]
+--- @field previousPosition Vector2
+--- @overload fun(owner: Actor, destination: Vector2): Move
 local Move = prism.Action:extend("Move")
-Move.name = "move"
 Move.targets = { MoveTarget }
 
 Move.requiredComponents = {
    prism.components.Controller,
-   prism.components.Mover
+   prism.components.Mover,
 }
 
 --- @param level Level
